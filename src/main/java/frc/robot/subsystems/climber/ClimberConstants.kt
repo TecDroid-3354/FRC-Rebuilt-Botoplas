8896058d3edf81cc2815dc9a549d656e982d3fbe
Santiago F. M. M. <santiagofernandomurguia@gmail.com>
2026-01-17 14:25:02 -0600

    Santiago Murguia: contants files updated

--- src/main/java/frc/robot/subsystems/climber/ClimberConstants.kt
+++ src/main/java/frc/robot/subsystems/climber/ClimberConstants.kt
@@ -1,6 +1,16 @@
 package frc.robot.subsystems.climber
 
 object ClimberConstants {
-        const val EXAMPLE_CONSTANT = 1
+        object IdConstants {
+                const val EXAMPLE_ID = 1
+        }
+
+        object PhysicalConstants {
+                const val EXAMPLE_RATIO = 7.0 / 3.0
+        }
+
+        object MotorConfigConstants {
+                const val EXAMPLE_AMP_LIMITS = 50.0
+        }
 
 }
