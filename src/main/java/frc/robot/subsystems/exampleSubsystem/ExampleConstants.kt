04a018d8d66de847aa222f67c56786a9dbcff2b8
Santiago F. M. M. <santiagofernandomurguia@gmail.com>
2026-01-17 13:29:02 -0600

    first commit

---
+++ src/main/java/frc/robot/subsystems/exampleSubsystem/ExampleConstants.kt
@@ -0,0 +1,8 @@
+package frc.robot.subsystems.exampleSubsystem
+
+object ExampleConstants {
+        // CAN IDs
+        const val MOTOR_ID = 1
+        const val MOTOR_CURRENT_LIMITS = 50.0
+
+}
---
+++ src/main/java/frc/robot/subsystems/exampleSubsystem/ExampleSubsystem.kt
@@ -0,0 +1,33 @@
+package frc.robot.subsystems.exampleSubsystem
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration
+import com.ctre.phoenix6.controls.VoltageOut
+import com.ctre.phoenix6.hardware.TalonFX
+import com.ctre.phoenix6.signals.NeutralModeValue
+import edu.wpi.first.units.measure.Voltage
+import edu.wpi.first.wpilibj2.command.Subsystem
+
+class Intake() : Subsystem {
+    private val motorController = TalonFX(ExampleConstants.MOTOR_ID)
+
+    init {
+        configureMotorInterface()
+    }
+
+    fun setVoltage(voltage: Voltage) {
+        val request = VoltageOut(voltage)
+        motorController.setControl(request)
+    }
+
+    private fun configureMotorInterface() {
+        val talonConfig = TalonFXConfiguration()
+        talonConfig.MotorOutput.withNeutralMode(NeutralModeValue.Brake)
+        talonConfig.CurrentLimits
+            .withSupplyCurrentLimitEnable(true)
+            .withSupplyCurrentLimit(ExampleConstants.MOTOR_CURRENT_LIMITS)
+
+
+        motorController.clearStickyFaults()
+        motorController.configurator.apply(talonConfig)
+    }
+}
