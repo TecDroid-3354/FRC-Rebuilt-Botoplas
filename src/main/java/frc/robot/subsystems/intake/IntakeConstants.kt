02e5ec93038e3079af2cb5ec34e37530bbb10d54
JPablo <A01713288@tec.mx>
2026-01-17 17:29:57 -0600

    Updated constants files with standard naming convention, now document's ready for delivery to collabs

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.kt
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.kt
@@ -1,15 +1,15 @@
 package frc.robot.subsystems.intake
 
 object IntakeConstants {
-        object IdConstants {
+        object Identification {
                 const val EXAMPLE_ID = 1
         }
 
-        object PhysicalConstants {
+        object PhysicalLimits {
                 const val EXAMPLE_RATIO = 7.0 / 3.0
         }
 
-        object MotorConfigConstants {
+        object Configuration {
                 const val EXAMPLE_AMP_LIMITS = 50.0
         }
 
