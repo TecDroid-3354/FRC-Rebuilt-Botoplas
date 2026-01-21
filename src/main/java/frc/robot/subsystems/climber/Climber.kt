2d8bdc4d10892be26581ed2198c5c677c7732f0d
Dandanapp27 <a01715546@tec.mx>
2026-01-20 20:55:09 -0600

    Added climber subsystem

--- src/main/java/frc/robot/subsystems/climber/Climber.kt
+++ src/main/java/frc/robot/subsystems/climber/Climber.kt
@@ -1,7 +1,68 @@
 package frc.robot.subsystems.climber
 
+import com.ctre.phoenix6.controls.Follower
+import com.ctre.phoenix6.controls.PositionVoltage
+import com.ctre.phoenix6.hardware.CANcoder
+import com.ctre.phoenix6.hardware.TalonFX
+import com.ctre.phoenix6.signals.MotorAlignmentValue
+import edu.wpi.first.units.measure.Angle
 import edu.wpi.first.wpilibj2.command.Subsystem
 
 class Climber() : Subsystem {
 
+    private val leader = TalonFX(ClimberConstants.Identification.LEADER_MOTOR_ID)
+    private val follower = TalonFX(ClimberConstants.Identification.FOLLOWER_MOTOR_ID)
+
+    private val absolute: CANcoder = CANcoder(ClimberConstants.Identification.ABSOLUTE_ID)
+
+    private val positionRequest = PositionVoltage(0.0)
+
+    init {
+        //Set "follower" motor to mimic "leader" motor
+        follower.setControl(Follower(leader.deviceID, MotorAlignmentValue.Opposed))
+
+        //Match relative encoder with absolute encoder
+        syncRelativeToAbsolute()
+    }
+
+    /**
+     * Moves the climber to the climb position
+     */
+    fun climb() {
+        val desiredPosition = ClimberConstants.PhysicalLimits.IDLE_POSITION
+
+        //val clampedPosition = ClimberConstants.PhysicalLimits.LIMITS_clamp()
+
+        val motorPosition = desiredPosition * ClimberConstants.PhysicalLimits.GEAR_RATIO
+
+        leader.setControl(positionRequest.withPosition(motorPosition))
+
+    }
+
+    /**
+     * Returns the climber to its idle (starting) position
+     */
+    fun idleClimb() {
+        val desiredPosition = ClimberConstants.PhysicalLimits.IDLE_POSITION
+
+        //val clampedPosition = ClimberConstants.PhysicalLimits.LIMITS_clamp()
+
+        val motorPosition = desiredPosition * ClimberConstants.PhysicalLimits.GEAR_RATIO
+
+        leader.setControl(positionRequest.withPosition(motorPosition))
+
+    }
+
+    fun getPosition(): Angle {
+        return leader.position.value * ClimberConstants.PhysicalLimits.GEAR_RATIO
+
+    }
+
+    /**
+     * Syncs the motors relative encoder to the absolute encoder
+     * Required for correct limit clamping
+     */
+    private fun syncRelativeToAbsolute() {
+        leader.setPosition(absolute.position.value.times(ClimberConstants.PhysicalLimits.GEAR_RATIO))
+    }
 }
--- src/main/java/frc/robot/subsystems/climber/ClimberConstants.kt
+++ src/main/java/frc/robot/subsystems/climber/ClimberConstants.kt
@@ -1,16 +1,30 @@
 package frc.robot.subsystems.climber
 
+import frc.template.utils.degrees
+import frc.template.utils.rotations
+import frc.template.utils.safety.MeasureLimits
+
 object ClimberConstants {
         object Identification {
-                const val EXAMPLE_ID = 1
+                const val LEADER_MOTOR_ID = 0
+                const val FOLLOWER_MOTOR_ID = 0
+                const val ABSOLUTE_ID = 0
+
         }
 
         object PhysicalLimits {
-                const val EXAMPLE_RATIO = 7.0 / 3.0
+                //Gear ratio (known later)
+                const val GEAR_RATIO = 7.0.rotations / 3.0.rotations
+
+                //Positions
+                const val IDLE_POSITION = 0.0.rotations
+                const val CLIMB_POSITION = 1.2.rotations
+
+                val LIMITS = MeasureLimits(0.0.rotations, 1.3.rotations)
         }
 
         object Configuration {
-                const val EXAMPLE_AMP_LIMITS = 50.0
+                const val CURRENT_LIMIT_AMPS = 50.0
         }
 
 }
